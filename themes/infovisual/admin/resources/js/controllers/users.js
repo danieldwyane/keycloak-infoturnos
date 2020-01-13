73e4db39f4ff30d221bbcef82797a6ca497574c2
Jose Daniel Buitrago Anamarima <jdbuitrago@itcol.com>
2020-01-13 12:35:51 -0500

    Req - Se corrigio traduccion, se cambio form en crear usuario y se quito grupo item menu

--- themes/infovisual/admin/resources/js/controllers/users.js
+++ themes/infovisual/admin/resources/js/controllers/users.js
@@ -625,7 +625,7 @@ module.controller('UserDetailCtrl', function($scope, realm, user, BruteForceUser
 	/**Seccion agregada por infovisual*/
     $http.get("http://172.16.11.98:8060/company/companies/")
 	  .then(function(response) {
-		  response.data.push({id:0,company:"Selecione"});
+		  response.data.push({id:0,company:"Seleccione..."});
 		  $scope.realm.companies = response.data;
 		  for(var i=0; i<response.data.length; i++){
 			if(response.data[i].id == user.attributes.company){
